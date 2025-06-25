074e7556581a576594f6adf72fbee1d5412553e9
dainiellim <dainiel.lim@gmail.com>
2025-06-25 18:04:13 +0800

    fix cors issue

--- src/be-app/src/main.ts
+++ src/be-app/src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+        "*"
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    optionsSuccessStatus: 204
+  });
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
